13e3d9faaf8137c1c9311b61084cbcf11dabbabc
tebybee <tbybee@mail.csuchico.edu>
2018-02-13 11:33:09 -0800

    Creates nickname based on first letters of provided first and last name.

--- main.cpp
+++ main.cpp
@@ -9,12 +9,18 @@ int main()
   //declare variables
   string first;
   string last;
+  string nickname;
 
   //get user input
-  cout<<"what is your first name?\n";
+  cout<<"What is your first name?\n";
   cin>> first;
   cout<<"What is your last name? \n";
   cin>> last;
+  nickname.push_back(first[0]);
+  nickname.push_back('.');
+  nickname.push_back(last[0]);
+  nickname.push_back('.');
+  cout<< "Welcome, " <<nickname<< ", here is your fortune...\n";
   //tell fortune
 
   return 0;
