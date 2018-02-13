4504c038d4b2d9e4c1e892051314e201d139d710
tebybee <tbybee@mail.csuchico.edu>
2018-02-13 11:10:42 -0800

    Prompts the user and assigns their first and last name as variables.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Travis Bybee
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,14 @@ using namespace std;
 int main()
 {
   //declare variables
+  string first;
+  string last;
 
   //get user input
-
+  cout<<"what is your first name?\n";
+  cin>> first;
+  cout<<"What is your last name? \n";
+  cin>> last;
   //tell fortune
 
   return 0;
