76cd6e5a2aaede1f4bdef5adc18fe9b2721b9823
tebybee <tbybee@mail.csuchico.edu>
2018-02-15 11:33:52 -0800

    records users first and last name, creates a nickname based on letters in their name, says the user is destined to be famous if the first letter of their first name begins with a vowel or tells the user to keep a low profile if not. It also tells the user that they have already met their true love if their last name ends with a vowel. It ends by telling the user to have a good day.

--- main.cpp
+++ main.cpp
@@ -22,6 +22,22 @@ int main()
   nickname.push_back('.');
   lucky = first.length();
   cout<< "Welcome, " <<nickname<< ", your lucky number is " <<lucky<<".\n";
+  char fort = first[0];
+  int len = last.length();
+  char love = last[len-1];
+  if(fort == 'a'||fort == 'e'||fort == 'i'||fort == 'o'||fort == 'u'||fort == 'A'||fort == 'E'||fort == 'I'||fort == 'O'||fort == 'U')
+  {
+    cout<<"You are destined to be famous!\n";
+  }
+  else
+  {
+    cout<<"You should keep a low profile.\n";
+  }
+  if(love == 'a'||love == 'e'||love == 'i'||love == 'o'||love =='u')
+  {
+    cout<<"You have already met your true love!\n";
+  }
+  cout<<"Have a good day!\n";
   //tell fortune
 
   return 0;
