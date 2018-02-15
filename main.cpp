ecac9026e3b14b15505abda34eafda7ba29c6188
tebybee <tbybee@mail.csuchico.edu>
2018-02-15 11:50:51 -0800

    takes users name, gives nickname, tells lucky number, fortune, and lover if applicable.

--- main.cpp
+++ main.cpp
@@ -22,24 +22,24 @@ int main()
   nickname.push_back('.');
   lucky = first.length();
   cout<< "Welcome, " <<nickname<< ", here is your fortune...\n";
-  cout<< "Your lucky number is " <<lucky<<".\n";
+  cout<< "your lucky number is " <<lucky<<"\n";
   char fort = first[0];
   int len = last.length();
   char love = last[len-1];
   if(fort == 'a'||fort == 'e'||fort == 'i'||fort == 'o'||fort == 'u'||fort == 'A'||fort == 'E'||fort == 'I'||fort == 'O'||fort == 'U')
   {
-    cout<<"You are destined to be famous!\n";
+    cout<<"you are destined to be famous!\n";
   }
   else
   {
-    cout<<"You should keep a low profile.\n";
+    cout<<"you should keep a low profile.\n";
   }
   if(love == 'a'||love == 'e'||love == 'i'||love == 'o'||love =='u')
   {
-    cout<<"You have already met your true love!\n";
+    cout<<"you have already met your true love!\n";
   }
-  cout<<"Have a good day!\n";
-  //tell fortune
+  cout<<"have a good day!\n";
+  
 
   return 0;
 }
